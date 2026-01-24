1a8e5909256c221b899ebbba0d63752254d69522
matcrown <mategonzalez0225@gmail.com>
2026-01-24 11:33:38 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,18 @@
-self.addEventListener('fetch', function(event) {
-  event.respondWith(fetch(event.request));
+const CACHE_NAME = 'kidom-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  'https://i.imgur.com/ZVERytB.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
 });
